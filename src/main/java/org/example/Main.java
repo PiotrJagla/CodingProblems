26b73aa39b36a977030c4a1e4e237da30aeb8381
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-11 21:04:28 +0200

    this is so smart plating bloom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,38 +7,22 @@ class Solution {
     public int earliestFullBloom(int[] plantTime, int[] growTime) {
         int result = 0;
 
-        int minPlantTime = Integer.MAX_VALUE;
-        int minPlantTimeIndex = -1;
         for(int i = 0 ; i < plantTime.length ; ++i){
-            if(plantTime[i] < minPlantTime){
-                minPlantTime = plantTime[i];
-                minPlantTimeIndex = i;
-            }
+            result += plantTime[i];
         }
-        List<Integer> plantingOrder = new ArrayList<>();
-        plantingOrder.add(minPlantTimeIndex);
 
-        for(int plant = 1 ; plant < plantTime.length ; ++plant){
-
-            int nextPlantIndex = -1;
-            int currentBestScore = 100;
-            for(int i = 0 ; i < plantTime.length ; ++i){
-                if(plantingOrder.contains(i))
-                    continue;
-
-                if(plantTime[i] - growTime[i] < currentBestScore){
-                    nextPlantIndex = i;
-                    currentBestScore = plantTime[i] - growTime[i];
+        for(int i = 0 ; i < growTime.length ; ++i){
+            int smallestIndex = i;
+            for(int j = i + 1 ; j < growTime.length  ; ++j){
+                if(growTime[smallestIndex] > growTime[j]){
+                    smallestIndex = j;
                 }
-
-
             }
-            plantingOrder.add(nextPlantIndex);
+            int temp = growTime[smallestIndex];
+            growTime[smallestIndex] = growTime[i];
+            growTime[i] = temp;
         }
-
-
-
-
+        result += growTime[0];
 
         return result;
     }
@@ -48,8 +32,8 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {1,2,3,2};
-        int[] arr2 = {2,1,2,1};
+        int[] arr = {27,5,24,17,27,4,23,16,6,26,13,17,21,3,9,10,28,26,4,10,28,2};
+        int[] arr2 = {26,9,14,17,6,14,23,24,11,6,27,14,13,1,15,5,12,15,23,27,28,12};
         System.out.println(s.earliestFullBloom(arr, arr2));
 
 
