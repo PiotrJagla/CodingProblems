2268dbbfdc9d3eb6aee95422dc1fddf2e67ef8e7
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-16 10:21:34 +0200

    dungeons 174 greedy form

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,33 +2,55 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Enumeration;
+
 
 class Solution {
-    public int maximumGap(int[] nums) {
-        if(nums.length < 2)
-            return 0;
+    public int calculateMinimumHP(int[][] dungeon) {
+        int healthLost = 0;
+        int minHealth = dungeon[0][0];
 
-        Arrays.sort(nums);
+        int Xpos = 0;
+        int Ypos = 0;
+        while(Xpos != dungeon[0].length - 1 || Ypos != dungeon.length - 1){
+            healthLost += dungeon[Ypos][Xpos];
+            if(healthLost < minHealth){
+                minHealth = healthLost;
+            }
+
+            if(Xpos == dungeon[0].length - 1){
+                Ypos++;
+            }
+            else if(Ypos == dungeon.length - 1){
+                Xpos++;
+            }
+            else if(dungeon[Ypos + 1][Xpos] >= dungeon[Ypos][Xpos + 1]){
+                Ypos++;
+            }
+            else if(dungeon[Ypos + 1][Xpos] < dungeon[Ypos][Xpos + 1]){
+                Xpos++;
+            }
 
 
-        int maxGap = 0;
-        for (int i = 0; i < nums.length - 1 ;i++) {
-            maxGap = Math.max(maxGap, Math.abs(nums[i] - nums[i+1]) );
         }
-        return maxGap;
+
+        healthLost += dungeon[Ypos][Xpos];
+        if(healthLost < minHealth){
+            minHealth = healthLost;
+        }
+
+        if(minHealth > 0)
+            return 1;
+
+        return Math.abs(minHealth) + 1;
     }
 }
 
 public class Main {
-
-
-
-
-
-
     public static void main(String[] args) {
 
-
-
+        Solution s = new Solution();
+        int[][] arr = {{-2,-3,3},{-5,-10,1},{10,30,-5}};
+        System.out.println(s.calculateMinimumHP(arr));
     }
 }
