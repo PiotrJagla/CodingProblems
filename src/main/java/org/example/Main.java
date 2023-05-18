2d93c16bd26f5f1acf766f9a442e814d2336b730
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-18 16:25:35 +0200

    final BFS solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,62 +1,29 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
+import javax.swing.plaf.InsetsUIResource;
+import java.util.*;
 
 
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1){
-            return true;
-        }
-        if(nums[0] == 0){
-            return false;
-        }
 
-        for (int i = 0; ; ) {
-            int maxJump = nums[i];
+        Queue<Integer> vertexesQueue = new LinkedList<>();
+        vertexesQueue.add(0);
+        boolean[] isVisited = new boolean[nums.length];
+        Arrays.fill(isVisited, false);
 
-            if(i + nums[i] >= nums.length - 1)
-                return true;
-
-            int maxNextJumpIndex = i + 1;
-            int maxNextJump = 0;
-            for(int j = i + 1 ; j <= maxJump + i ; ++j){
-                if(maxNextJump <= nums[j]){
-                    int oneJumpFurther = findMaxJump(nums,j);
-                    if(oneJumpFurther + nums[oneJumpFurther] >= nums.length - 1)
-                        return true;
-                    if(j != maxJump + i && nums[oneJumpFurther] == 0)
-                        continue;
-                    maxNextJumpIndex = j;
-                    maxNextJump = nums[j];
+        while(vertexesQueue.isEmpty() == false){
+            int currentVertex = vertexesQueue.peek();
+            vertexesQueue.remove();
+            for (int adjacent = currentVertex; adjacent < nums.length && adjacent <= currentVertex + nums[currentVertex]; adjacent++) {
+                if(isVisited[adjacent] == false){
+                    isVisited[adjacent] = true;
+                    vertexesQueue.add(adjacent);
                 }
             }
-            i = maxNextJumpIndex;
-
-            if(nums[maxNextJumpIndex] == 0)
-                return false;
         }
-    }
 
-    private int findMaxJump(int nums[], int startIndex){
-        int maxNextJumpIndex = startIndex + 1;
-        if(nums[startIndex] + startIndex >= nums.length - 1)
-            return nums.length - 1;
-        if(nums[startIndex] == 0){
-            return startIndex;
-        }
-        if(maxNextJumpIndex >= nums.length-1)
-            return startIndex;
-
-        int maxJump = nums[startIndex];
-        for(int j = startIndex + 2 ; j <= maxJump + startIndex && j <= nums.length - 2 ; ++j){
-            if(nums[maxNextJumpIndex] <= nums[j]){
-                maxNextJumpIndex = j;
-            }
-        }
-        return maxNextJumpIndex;
+        return isVisited[nums.length-1];
     }
 }
 
