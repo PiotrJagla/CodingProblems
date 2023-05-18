bce8015f9f40d9aa8b5b908d90556f99ec509592
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-18 16:15:00 +0200

    jump game 1, my greedy approach

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,9 +21,16 @@ class Solution {
                 return true;
 
             int maxNextJumpIndex = i + 1;
-            for(int j = i + 2 ; j <= maxJump + i ; ++j){
-                if(nums[maxNextJumpIndex] <= nums[j]){
+            int maxNextJump = 0;
+            for(int j = i + 1 ; j <= maxJump + i ; ++j){
+                if(maxNextJump <= nums[j]){
+                    int oneJumpFurther = findMaxJump(nums,j);
+                    if(oneJumpFurther + nums[oneJumpFurther] >= nums.length - 1)
+                        return true;
+                    if(j != maxJump + i && nums[oneJumpFurther] == 0)
+                        continue;
                     maxNextJumpIndex = j;
+                    maxNextJump = nums[j];
                 }
             }
             i = maxNextJumpIndex;
@@ -32,14 +39,35 @@ class Solution {
                 return false;
         }
     }
+
+    private int findMaxJump(int nums[], int startIndex){
+        int maxNextJumpIndex = startIndex + 1;
+        if(nums[startIndex] + startIndex >= nums.length - 1)
+            return nums.length - 1;
+        if(nums[startIndex] == 0){
+            return startIndex;
+        }
+        if(maxNextJumpIndex >= nums.length-1)
+            return startIndex;
+
+        int maxJump = nums[startIndex];
+        for(int j = startIndex + 2 ; j <= maxJump + startIndex && j <= nums.length - 2 ; ++j){
+            if(nums[maxNextJumpIndex] <= nums[j]){
+                maxNextJumpIndex = j;
+            }
+        }
+        return maxNextJumpIndex;
+    }
 }
+
+
 
 public class Main {
     public static void main(String[] args) {
 
         Solution s = new Solution();
         int[][] arr = {{-2,-3,3},{-5,-10,1},{10,30,-5}};
-        int[] arrr = {4,2,0,0,1,1,4,4,4,0,4,0};
+        int[] arrr = {1,1,2,2,0,1,1};
         System.out.println(s.canJump(arrr));
     }
 }
