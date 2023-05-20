1b0202bcbcb8102d9944762373506440e37bd101
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-20 21:29:18 +0200

    playing with multithreading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,30 @@
 package org.example;
 
-import javax.swing.plaf.InsetsUIResource;
-import java.util.*;
+class MultiThreadThing extends Thread{
 
-import java.util.function.Predicate;
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
 
 public class Main {
-    public static int totalValue(List<Integer> values,Predicate<Integer> selector){
-        return values.stream()
-                .filter(selector)
-                .mapToInt(e -> e)
-                .sum();
-    }
+
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
-        System.out.println(totalValue(numbers, e -> true));
-        System.out.println(totalValue(numbers, e -> e%2 == 0));
-        System.out.println(totalValue(numbers, e -> e%2 != 0));
+        MultiThreadThing myThing = new MultiThreadThing();
+        MultiThreadThing myThing2 = new MultiThreadThing();
+
+
+        myThing.start();
+        myThing2.run();
 
     }
 }
