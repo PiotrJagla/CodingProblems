c4f1e4904efde5f02d7cf6679676206b9cb53f5e
PiotrJagielny <piotrek2002.09@gmail.com>
2023-05-22 20:24:10 +0200

    first interval approach, this solution is bad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,44 +1,158 @@
 package org.example;
 
-class MultiThreadThing implements Runnable{
+import java.util.Arrays;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if(intervals.length == 0){
+            return new int[][]{{newInterval[0], newInterval[1]}};
+        }
+        else if(intervals.length == 1){
+
+            if(newInterval[1]  < intervals[0][0]){
+                return new int[][]{newInterval,intervals[0]};
+            }
+            else if(newInterval[0] > intervals[0][1]){
+                return new int[][]{intervals[0], newInterval};
+            }
+            else{
+                return new int[][]{{
+                        Math.min(intervals[0][0], newInterval[0]), Math.max(newInterval[1], intervals[0][1])
+                }};
+            }
 
 
+        }
 
-    private int threadNumber;
-    public MultiThreadThing(int threadNumber) {
-        this.threadNumber = threadNumber;
-    }
 
-    @Override
-    public void run() {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i + " from thread " + threadNumber);
+        int[][] result = new int[1][1];
+        for (int i = intervals.length - 1; i >= 0; i--) {
+            if(intervals[i][0] <= newInterval[0] ){
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                if(newInterval[0] > intervals[i][1] && newInterval[1] < intervals[i+1][0]){
+                    //Interval dont overlap
+                    result = new int[intervals.length + 1][2];
+                    for (int j = 0; j < result.length; j++) {
+                        if(i + 1 == j){
+                            result[j][0] = newInterval[0];
+                            result[j][1] = newInterval[1];
+                        }
+                        else if( j <= i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+
+                        }
+                        else if(j > i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+                        }
+
+                    }
+                    return result;
+
+                }
+                else if(newInterval[1] < intervals[i + 1][0] && newInterval[0] <= intervals[i][1]){
+                    //new interval overlap with only previous interval
+                    result = new int[intervals.length][2];
+                    for (int j = 0; j < result.length; j++) {
+                        if(i == j){
+                            result[j][0] = intervals[i][0];
+                            result[j][1] = newInterval[1];
+                        }
+                        else if( j < i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+
+                        }
+                        else if(j > i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+                        }
+
+                    }
+                    return result;
+                }
+                else if(newInterval[0] > intervals[i][1] && newInterval[1] >= intervals[i+1][0]){
+                    //new interval dont overtlap with previous, but overlap with next interval/s
+                    int intervalsOverlap = 1;
+                    for (int j = i + 2; j < intervals.length; j++) {
+                        if(newInterval[1] >= intervals[j][0]){
+                            intervalsOverlap++;
+                        }
+                        else
+                            break;
+                    }
+
+                    result = new int[intervals.length - intervalsOverlap + 1][2];
+                    for (int j = 0; j < result.length; j++) {
+                        if(i + 1 == j){
+                            result[j][0] = newInterval[0];
+                            result[j][1] = intervals[i + intervalsOverlap][1];
+
+                        }
+                        else if( j <= i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+
+                        }
+                        else if(j > i){
+                            result[j][0] = intervals[j + intervalsOverlap - 1][0];
+                            result[j][1] = intervals[j +intervalsOverlap - 1][1];
+                        }
+
+                    }
+                    return result;
+                }
+                else if(newInterval[0] <= intervals[i][1] && newInterval[1] >= intervals[i+1][0]){
+                    //new interval overlap with previous and next interval
+                    int intervalsOverlap = 1;
+                    result = new int[intervals.length - intervalsOverlap][2];
+                    for (int j = i + 2; j < intervals.length; j++) {
+                        if(newInterval[1] >= intervals[j][0]){
+                            intervalsOverlap++;
+                        }
+                        else
+                            break;
+                    }
+
+                    for (int j = 0; j < result.length; j++) {
+                        if(i == j){
+                            result[j][0] = intervals[i][0];
+                            result[j][1] = intervals[i + intervalsOverlap][1];
+                        }
+                        else if( j < i){
+                            result[j][0] = intervals[j][0];
+                            result[j][1] = intervals[j][1];
+
+                        }
+                        else if(j > i){
+                            result[j][0] = intervals[j + intervalsOverlap][0];
+                            result[j][1] = intervals[j + intervalsOverlap][1];
+                        }
+
+                    }
+                    if(result.length == 1){
+                        result[0][0] = intervals[i][0];
+                        result[0][1] = intervals[i + intervalsOverlap][1];
+                    }
+                    return result;
+                }
+
             }
         }
+
+        return result;
     }
 }
-
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 4; i++) {
-            MultiThreadThing myThing = new MultiThreadThing(i);
-            Thread myThread = new Thread(myThing);
-            myThread.setPriority(i + 1);
-            myThread.start();
 
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {5,6};
+        int[][] arrr = {{1,5},{6,8}};
 
-        }
-        System.out.println(Thread.activeCount());
-        System.out.println(Thread.currentThread().getName());
-
-
-
-
+        System.out.println(Arrays.deepToString(s.insert(arrr,arr)));
     }
+
 }
